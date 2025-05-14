69c8460be2db7cded8fa1a8e1c5244cf688444a6
Lazlo <lazlopaul17@gmail.com>
2025-05-14 11:17:46 -0700

    changing for updated display

--- ven/app.py
+++ ven/app.py
@@ -16,9 +16,11 @@ import numpy as np
 
 app = Flask(__name__)
 
+VEN_TYPE = "ev" # "hvac, EV, or battery"
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template(f"{VEN_TYPE}.html")
 
 
 # dummy data
@@ -28,19 +30,39 @@ def _throttle_generator():
         yield round(i%24/24, 2)
         i = i + 1
 
+def _hour_generator():
+    i = 0
+    while True:
+        yield i%24
+        i = i + 1
+
 throttle = _throttle_generator()
 def _get_current_throttle_amt():
     return next(throttle)
 
 prices = _throttle_generator()
 def _get_current_event_price():
-    return next(prices)
+    return 1 - next(prices)
+
+hours = _hour_generator()
+def _get_current_hour():
+    return next(hours)
+def create_date_string(hour):
+    # Use today's date as the base date
+    base_date = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+
+    # Adjust the hour and format as ISO 8601 with milliseconds and 'Z'
+    date_string = (base_date + timedelta(hours=hour)).strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
+    
+    return date_string
+
 
 @app.route('/chart_data')
 def data():
     # Get current price data from the event
     current_price = _get_current_event_price()
     throttle = _get_current_throttle_amt()    
+    hour = _get_current_hour()
     # Calculate min and max for gauge range
     # Using a buffer of 20% below min and above max for better visualization
     min_price = 0
@@ -53,7 +75,8 @@ def data():
         "max": max_price,
         "currentThrottle": throttle,
         "minThrottle": 0,
-        "maxThrottle": 1
+        "maxThrottle": 1,
+        "currentHour": hour
     }
     
     return jsonify(gauge_data)
